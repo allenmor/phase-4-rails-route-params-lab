b54d7d43d38b4b6fbb8fa4ddfaae68f7eea224f9
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-18 15:22:32 -0400

    commit tree 405e3e84e93701966fb0b5e7ad67e869d04e91ef

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    
+    students = params[:name] ? 
+    Student.find_by(first_name: params[:name].capitalize()) || Student.find_by(last_name: params[:name].capitalize()) :
+    Student.all
     render json: students
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
